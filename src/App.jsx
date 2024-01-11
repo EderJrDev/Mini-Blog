ae31718179aa28800801e58c06c645dced6c8403
EderJrDev <ederjuninho2003@gmail.com>
2024-01-10 22:26:31 -0300

    feat: add new folders, and create css to footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>MiniBlog</h1>
+      <div className="App">
+
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh9_M2en47ZyK9N2UDNpvxsyqrljbzZiQ",
+  authDomain: "miniblog-5d5d1.firebaseapp.com",
+  projectId: "miniblog-5d5d1",
+  storageBucket: "miniblog-5d5d1.appspot.com",
+  messagingSenderId: "310371885859",
+  appId: "1:310371885859:web:67ed4d8ac4253fe7ffe296",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
