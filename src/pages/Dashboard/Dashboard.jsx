859af2f3737a8a1a4a99b083128a43822251b0f7
Eder Jr <ederjuninho2003@gmail.com>
2024-01-31 17:58:57 -0300

    feat: add posts in dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,43 @@
 //CSS
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 import styles from "./Dashboard.module.css";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  Ver
+                </Link>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,5 +1,4 @@
 //CSS
-import { useState } from "react";
 import styles from "./Post.module.css";
 import { useParams } from "react-router-dom";
 import { useFetchDocument } from "../../hooks/useFetchDocument";
@@ -9,19 +8,23 @@ const Post = () => {
   const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
+    <div className={styles.post_container}>
       {loading && <p>Carregando post...</p>}
-      {/* {post && (
+      {post && (
         <>
           <h1>{post.title}</h1>
           <img src={post.image} alt={post.title} />
           <p>{post.body}</p>
           <h3>Esse post trata sobre: </h3>
-          {post.tagsArray.map(tag) => (
-
-          )}
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+              <p key={tag}>
+                <span>#</span> {tag}
+              </p>
+            ))}
+          </div>
         </>
-      )} */}
+      )}
     </div>
   );
 };
