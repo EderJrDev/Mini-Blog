eb95dbb39587117f5a53f0191294306c49cc229b
Eder Jr <ederjuninho2003@gmail.com>
2024-02-01 16:35:40 -0300

    feat: add options to  delete and edit post

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 import styles from "./Dashboard.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocuments";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const { deleteDocument } = useDeleteDocument("posts");
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -22,20 +30,33 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
-          {posts.map((post) => (
-            <div key={post.id}>
-              <p>{post.title}</p>
-              <div>
-                <Link to={`/posts/${post.id}`} className="btn btn-outline">
-                  Ver
-                </Link>
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </>
       )}
     </div>
